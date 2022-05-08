992b9a7df6e6f5849e7c325154433928f41441e2
Tom McLaughlin <tom@codedown.io>
2022-05-08 00:03:58 -0700

    Cleaning up and renaming

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import typescript from 'rollup-plugin-typescript';
 import pkg from './package.json';
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript';
 
 export default [
 	// browser-friendly UMD build
 	{
 		input: 'src/main.ts',
 		output: {
-			name: 'howLongUntilLunch',
+			name: 'immutability-helper-rfc6902',
 			file: pkg.browser,
 			format: 'umd'
 		},
@@ -23,7 +23,7 @@ export default [
 	// (We could have three entries in the configuration array
 	// instead of two, but it's quicker to generate multiple
 	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify 
+	// an array for the `output` option, where we can specify
 	// `file` and `format` for each target)
 	{
 		input: 'src/main.ts',
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
-import ms from 'ms';
-import lunchtime from './lunchtime.js';
-import millisecondsUntil from './millisecondsUntil.js';
 
-export default function howLongUntilLunch(hours: number = 12, minutes: number = 30): string {
-	const millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
-	return ms(millisecondsUntilLunchTime, { long: true });
+export default function patch(value: any, patches: any[]): any {
+	return null;
 }
